912a5c1ee535bfaef497d9e14c525e90d9ecc7e6
zikos2 <zakaria2ettani@gmail.com>
2022-05-05 20:35:11 +0000

    capturing image & recovering metadata

--- WEB/FRONTEND/demo/src/App.js
+++ WEB/FRONTEND/demo/src/App.js
@@ -1,33 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import styled from "styled-components";
 
-import NavBar from './components/NavBar'
-import Root from './components/Root'
-import Home from './components/Home'
-import Docs from './components/Docs'
-import Footer from './components/Footer'
+import NavBar from "./components/NavBar";
+import Root from "./components/Root";
+import Annotate from "./components/Annotate";
+import Footer from "./components/Footer";
 
 const Main = styled.main`
   margin: 0 16px;
   margin-top: 51px;
-`
+`;
 
 export default () => (
-  <Router basename='/react-image-annotation'>
+  <Router basename="/react-image-annotation">
     <Root>
-
       <Main>
-        <Route
-          exact
-          path='/'
-          component={Home}
-        />
-        <Route
-          path='/docs'
-          component={Docs}
-        />
+        <Route path="/annotate" component={Annotate} />
       </Main>
     </Root>
   </Router>
-)
+);
