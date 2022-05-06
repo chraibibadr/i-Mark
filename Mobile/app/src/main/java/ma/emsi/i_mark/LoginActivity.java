3fe213af16552c6896e2c5166888dd93b6cc4e5a
Elouaji <elouajisoukaina0@gmail.com>
2022-05-06 17:11:19 +0000

    Login_Design

---
+++ Mobile/app/src/main/java/ma/emsi/i_mark/LoginActivity.java
@@ -0,0 +1,16 @@
+package ma.emsi.i_mark;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+}
+
+
---
+++ Mobile/app/src/main/java/ma/emsi/i_mark/RegisterActivity.java
@@ -0,0 +1,14 @@
+package ma.emsi.i_mark;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+}
