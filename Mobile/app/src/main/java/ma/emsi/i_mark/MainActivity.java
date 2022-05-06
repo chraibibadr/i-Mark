40e4ca5a862e4ef477688310d883287c2765c008
BADER <badr.chraibibadr@gmail.com>
2022-05-06 23:52:45 +0000

    Amelioration d'annotation

--- Mobile/app/src/main/java/ma/emsi/i_mark/MainActivity.java
+++ Mobile/app/src/main/java/ma/emsi/i_mark/MainActivity.java
@@ -3,12 +3,14 @@ package ma.emsi.i_mark;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -20,29 +22,49 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
+
+    String insertUrl = "https://i-mark.herokuapp.com/images";
 
     private Button confirm, open;
     private SimpleDrawingView img;
     Drawable d;
 
-    private double latitude;
-    private double longitude;
+    private double latitude, longitude;
 
-    String height, width;
+    String height, width, tag;
+
+    String encodeImageString;
 
     private String[] PERMISSIONS;
 
@@ -85,17 +107,92 @@ public class MainActivity extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Date currentTime = Calendar.getInstance().getTime();
-                DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
-                Log.d("width", width);
-                Log.d("height", height);
-                Log.d("date_captured", currentTime.toString());
-                Log.d("latitude", latitude+"");
-                Log.d("longitude", longitude+"");
-                Log.d("bbox", img.getBbox().toString());
-                Toast.makeText(MainActivity.this, "width : "+width+"\nheight : "+height+"\ndate_captured : "+dateFormat.format(currentTime)+"\nlatitude : "+latitude+"\nlongitude : "+longitude+"\nbbox : \n"+img.getBbox(), Toast.LENGTH_LONG).show();
+
+                AlertDialog.Builder annotator = new AlertDialog.Builder(MainActivity.this);
+                annotator.setTitle("Saisir l'annotation !");
+
+                final EditText annotation = new EditText(MainActivity.this);
+                annotator.setView(annotation);
+
+                annotator.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tag = annotation.getText().toString();
+                        Date currentTime = Calendar.getInstance().getTime();
+                        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+                        Log.d("width", width);
+                        Log.d("height", height);
+                        Log.d("date_captured", currentTime.toString());
+                        Log.d("latitude", latitude + "");
+                        Log.d("longitude", longitude + "");
+                        Log.d("annotation", tag);
+                        Log.d("bbox", img.getBbox().toString());
+                        Log.d("image", encodeImageString);
+                        Toast.makeText(MainActivity.this, "width : " + width + "\nheight : " + height + "\ndate_captured : " + dateFormat.format(currentTime) + "\nlatitude : " + latitude + "\nlongitude : " + longitude + "\nannotation : " + tag + "\nbbox : \n" + img.getBbox(), Toast.LENGTH_LONG).show();
+
+                        JSONObject image = new JSONObject();
+                        JSONObject gps_location = new JSONObject();
+                        JSONObject object = new JSONObject();
+                        JSONArray objects = new JSONArray();
+                        ArrayList<JSONObject> objectsArray = new ArrayList();
+
+                        try {
+                            image.put("file_url", tag);
+                            image.put("width", width);
+                            image.put("height", height);
+
+                            gps_location.put("latitude", latitude);
+                            gps_location.put("longitude", longitude);
+
+                            image.put("gps_location", gps_location);
+
+                            object.put("annotation",tag);
+                            object.put("bbox",img.getBbox().toString());
+
+                            objectsArray.add(object);
+
+                            image.put("objects", objectsArray);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Log.d("JSON OBJECT",image.toString());
+
+                    }
+                });
+                annotator.show();
             }
         });
+    }
+
+    void addAnnotation(JSONObject img) {
+         // Make request for JSONObject
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST, insertUrl, img,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("MainActivity", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MainActivity", error.toString());
+            }
+        }) {
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+        // Adding request to request queue
+        Volley.newRequestQueue(this).add(jsonObjReq);
     }
 
     @Override
@@ -106,6 +203,7 @@ public class MainActivity extends AppCompatActivity {
         img.setBackground(d);
         height = photo.getHeight() + "";
         width = photo.getWidth() + "";
+        encodeBitmapImage(photo);
     }
 
     private boolean hasPermissions(Context context, String... PERMISSIONS) {
@@ -141,5 +239,12 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Camera Permission is denied", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void encodeBitmapImage(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] bytesofimage = byteArrayOutputStream.toByteArray();
+        encodeImageString = android.util.Base64.encodeToString(bytesofimage, Base64.DEFAULT);
     }
 }
--- Mobile/app/src/main/java/ma/emsi/i_mark/RegisterActivity.java
+++ Mobile/app/src/main/java/ma/emsi/i_mark/RegisterActivity.java
@@ -11,4 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_main);
+    }
 }
